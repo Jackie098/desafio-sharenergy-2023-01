1e67ed9b74d5c47d0d3e621dbfc549aaf05accfc
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-01-15 18:30:57 -0300

    bugFIx: decreased cache time in configuration react query

--- web/src/services/queryClient.ts
+++ web/src/services/queryClient.ts
@@ -4,7 +4,7 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      cacheTime: 10 * 1000,
+      cacheTime: 4 * 1000,
       staleTime: 1 * 1000 * 60, // in minutes
       retry: 2,
     },
