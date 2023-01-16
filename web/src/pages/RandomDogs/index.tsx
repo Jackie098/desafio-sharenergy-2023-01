42635328f876a9c62487d748b3bfa8fc8245a851
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-01-16 12:18:29 -0300

    style(dog): added some styling and loaders

--- web/src/pages/RandomDogs/index.tsx
+++ web/src/pages/RandomDogs/index.tsx
@@ -5,24 +5,36 @@ import { randomDog } from "../../services/dogs";
 import Resizer from "react-image-file-resizer";
 import axios, { AxiosResponse } from "axios";
 import { GREEN_500, PRIMARY } from "../../utils/colors";
+import { CircularProgress, Typography } from "@mui/material";
+import { useState } from "react";
 
 export function RandomDogs() {
   const queryClient = useQueryClient();
 
-  const { data: dogUrl } = useQuery("randomicDog", async () => {
-    const { url } = await randomDog();
+  const [isResizingImage, setIsResizingImage] = useState(false);
 
-    const urlToBlob: AxiosResponse<Blob> = await axios.get(url, {
-      responseType: "blob",
-    });
+  const {
+    data: dogUrl,
+    isLoading,
+    isError,
+  } = useQuery("randomicDog", async () => {
+    try {
+      const { url } = await randomDog();
 
-    const resizedImage = await resizeFile(urlToBlob.data);
+      setIsResizingImage(true);
+      const urlToBlob: AxiosResponse<Blob> = await axios.get(url, {
+        responseType: "blob",
+      });
 
-    console.log("url", url);
-    console.log("urlToBlob", urlToBlob);
-    console.log("resizedImage", resizedImage);
+      const resizedImage = await resizeFile(urlToBlob.data);
 
-    return resizedImage;
+      return resizedImage;
+    } catch (error) {
+      // TODO dog: add a toast error here
+      throw new Error("Error while request or resizing dog image");
+    } finally {
+      setIsResizingImage(false);
+    }
   });
 
   const resizeFile = (
@@ -50,6 +62,7 @@ export function RandomDogs() {
   return (
     <Box display="flex" flexDirection={"column"} alignItems={"center"}>
       <Button
+        disabled={isLoading || isResizingImage}
         onClick={handleClick}
         variant="contained"
         size="large"
@@ -57,29 +70,46 @@ export function RandomDogs() {
           padding: "8px 48px",
           background:
             "linear-gradient(90deg, rgba(0,162,162,1) 0%, rgba(0,162,162,1) 13%, rgba(211,217,41,1) 100%)",
-          // background:
-          //TODO: Background gradient
+          opacity: isLoading || isResizingImage ? 0.4 : 1,
         }}
       >
-        Other Dog
+        {isLoading || isResizingImage ? (
+          <CircularProgress
+            size={20}
+            sx={{
+              color: "#FFF",
+            }}
+          />
+        ) : (
+          <Typography fontWeight={600}>Other Dog</Typography>
+        )}
       </Button>
-      <img
-        src={dogUrl as string}
-        alt="Image of a random dog"
-        style={{
-          maxWidth: "800px",
-          maxHeight: "600px",
-          height: "100%",
-          objectFit: "cover",
-          borderLeft: `10px ${GREEN_500} solid`,
-          borderTop: `10px ${GREEN_500} solid`,
-          borderRight: `10px ${PRIMARY} solid`,
-          borderBottom: `10px ${PRIMARY} solid`,
+      {isLoading ? (
+        <CircularProgress
+          size={20}
+          sx={{
+            marginTop: "64px",
+          }}
+        />
+      ) : (
+        <img
+          src={dogUrl as string}
+          alt="Image of a random dog"
+          style={{
+            maxWidth: "800px",
+            maxHeight: "600px",
+            height: "100%",
+            objectFit: "cover",
+            borderLeft: `10px ${GREEN_500} solid`,
+            borderTop: `10px ${GREEN_500} solid`,
+            borderRight: `10px ${PRIMARY} solid`,
+            borderBottom: `10px ${PRIMARY} solid`,
 
-          width: "85%",
-          marginTop: "54px",
-        }}
-      />
+            width: "85%",
+            marginTop: "54px",
+          }}
+        />
+      )}
     </Box>
   );
 }
