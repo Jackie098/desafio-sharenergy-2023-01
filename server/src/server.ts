4d6d64407d5066042cf3a7e7c42b9b6c4be6b472
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-01-19 21:27:09 -0300

    feat: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+
 import { router } from "./routes";
 
 import dotenv from "dotenv";
@@ -6,6 +8,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
