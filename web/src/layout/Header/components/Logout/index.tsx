ba66cba7c6b8dedbcda27e6fa9c07dccd652d370
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-01-20 20:58:25 -0300

    feat: redirecting to login page when user is logged out

--- web/src/layout/Header/components/Logout/index.tsx
+++ web/src/layout/Header/components/Logout/index.tsx
@@ -1,8 +1,19 @@
 import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../../hooks/useAuth";
 
 export function Logout() {
   const { signOut } = useAuth();
+  const navigate = useNavigate();
 
-  return <Button onClick={() => signOut()}>Logout</Button>;
+  return (
+    <Button
+      onClick={() => {
+        signOut();
+        navigate("/");
+      }}
+    >
+      Logout
+    </Button>
+  );
 }
